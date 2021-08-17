9775a7b90fd128ae997686a6ca7399d699ab19bc
koftov <ahronlu@gmail.com>
2021-08-17 20:30:19 +0300

    prepare to deploy to heroku

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
@@ -15,6 +16,16 @@ app.use(express.json());
 
 // Define Routes
 app.use("/getRestaurantList", require("./routes/restaurants"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
